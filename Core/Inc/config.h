1cde0fa38f13d2eb0330a9f3368f30c2bc8a25ba
tekinis <108182132+tekinis@users.noreply.github.com>
2023-05-21 22:36:28 +0300

    Update config.h

--- Core/Inc/config.h
+++ Core/Inc/config.h
@@ -47,20 +47,20 @@
 #define GEAR_RATIO 11 //15 for original M365 motor
 
 // speed limits for invividual modes in kph
-#define SPEEDLIMIT_ECO 6
-#define SPEEDLIMIT_NORMAL 20
+#define SPEEDLIMIT_ECO 15
+#define SPEEDLIMIT_NORMAL 35
 #define SPEEDLIMIT_SPORT 50
 
 // motor current limits for invividual modes in mA, see default settings at https://max.cfw.sh/#
-#define PH_CURRENT_MAX_ECO 16000
-#define PH_CURRENT_MAX_NORMAL 28000
-#define PH_CURRENT_MAX_SPORT 55000
+#define PH_CURRENT_MAX_ECO 10000
+#define PH_CURRENT_MAX_NORMAL 15000
+#define PH_CURRENT_MAX_SPORT 25000
 
 // motor current limit for regen in mA
-#define REGEN_CURRENT 20000
+#define REGEN_CURRENT 15000
 
 // maximum current for flux weakening in mA
-#define FW_CURRENT_MAX 18000 //max id
+#define FW_CURRENT_MAX 5000 //max id
 
 // maximum battery currents in mA
 #define BATTERYCURRENT_MAX 14500
